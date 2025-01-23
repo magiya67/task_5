54f636b717c5ebd87241b4cbe937774e1bcf6ce8
magiya67 <151738190+magiya67@users.noreply.github.com>
2025-01-23 17:54:45 +1000

    meow

--- Task_5/settings.py
+++ Task_5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x%+z+23q8)=jx_86)hu%%_mc==g10-(ut%j#%tjdzyq3y&z0w5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['192.168.0.14']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
