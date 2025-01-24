f23606028df5d40374b3a8dc0b5be0b35f90238d
magiya67 <151738190+magiya67@users.noreply.github.com>
2025-01-24 14:44:07 +1000

    Add files via upload

--- app5/views.py
+++ app5/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import CustomUser
 from .serializers import UserSerializer
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 class userCreating(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-
+@csrf_exempt
 class UserRUD(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
